45f4d8ae52d7ab1ce52f000b0f92ee32833c27ef
Bintang Bagus Pangestu <bintangbgs16@gmail.com>
2025-02-16 16:04:15 +0700

    add error handler

--- database/index.js
+++ database/index.js
@@ -3,7 +3,9 @@ const dotenv = require('dotenv');
 dotenv.config();
 // const { dbHost, dbPass, dbName, dbPort, dbUser } = require('../app/config');
 
-mongoose.connect(`${process.env.MONGODB_URI}`);
+mongoose
+  .connect(`${process.env.MONGODB_URI}`)
+  .catch((error) => console.log(error));
 
 const db = mongoose.connection;
 
