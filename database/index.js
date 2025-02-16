9a8e596fc57a9b6166d8577daf75b5b61c8d5496
Bintang Bagus Pangestu <bintangbgs16@gmail.com>
2025-02-16 15:10:46 +0700

    test connection database

--- database/index.js
+++ database/index.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
-const { dbHost, dbPass, dbName, dbPort, dbUser } = require('../app/config');
+const dotenv = require('dotenv');
+dotenv.config();
+// const { dbHost, dbPass, dbName, dbPort, dbUser } = require('../app/config');
 
-mongoose.connect(
-  `mongodb+srv://${dbUser}:${dbPass}@${dbHost}/${dbName}?retryWrites=true&w=majority&appName=React-X-Express`
-);
+mongoose.connect(process.env.MONGODB_URI);
 
 const db = mongoose.connection;
 
